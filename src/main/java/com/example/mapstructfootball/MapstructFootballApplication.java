9a9a34468869bbfe8e3890bdf4c117da6b5894cf
Yurii <granickiy98@gmail.com>
2023-01-25 01:55:27 +0200

    Created package

---
+++ src/main/java/com/example/mapstructfootball/MapstructFootballApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapstructfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructFootballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructFootballApplication.class, args);
+    }
+
+}
